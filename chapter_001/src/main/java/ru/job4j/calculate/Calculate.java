fd768c738a13ebe82db9312eec7823b787c1aa54
MaximBerlin <lackdoc@yandex.ru>
2019-04-22 01:24:25 +0200

    1.3. JavaDoc. Code convention[#135044]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
-
+/**
+* Package for calculate task.
+*
+* @author Skakun Maxim (lackdoc@yandex.ru)
+* @version 0.1
+* @since 22.04.19
+*/
 public class Calculate{
-
+/**
+* Main.
+* @param args - args
+ */
     public static void main (String[] args){
         System.out.println("Hello World");
     }
