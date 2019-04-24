d5a342886159195963b4e92a3d5dd68161bbc6c1
MaximBerlin <lackdoc@yandex.ru>
2019-04-24 20:49:00 +0200

    1.4. JUnit[#135045]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,26 +1,18 @@
-package ru.job4j.calculate;
+package ru.job4j;
+
 /**
- * Package for calculate task.
+ * Calculate.
  *
- * @author Skakun Maxim (lackdoc@yandex.ru)
- * @version 0.1
- * @since 22.04.19
+ * @author Skakun Maxim (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
  */
-public class Calculate{
+public class Calculate {
     /**
      * Main.
-     * @param args - args
+     * @param args - args.
      */
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
-    /**
-     * Method echo.
-     * @param name Your name.
-     * @return Echo plus your name.
-     */
-    public String echo(String name) {
-        return "Echo, echo, echo : " + name;
-    }
-
 }
