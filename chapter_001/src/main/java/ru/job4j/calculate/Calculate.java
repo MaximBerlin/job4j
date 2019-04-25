8a6feacff59e60f398845d27f9afe8dcbc10596d
MaximBerlin <lackdoc@yandex.ru>
2019-04-25 22:43:17 +0200

    1.3.1 Maven Plugins[#135040]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,33 +1,18 @@
 package ru.job4j.calculate;
+
 /**
  * Calculate.
  *
- * @author Skakun Maxim (parsentev@yandex.ru)
+ * @author Skakun Maxim (lackdoc@yandex.ru)
  * @version $Id$
  * @since 0.1
  */
 public class Calculate {
     /**
-     * add.
-     * double
+     * Main.
+     * @param args - args.
      */
-    public double add(double first, double second) {
-        double summeAdd = first + second;
-        return summeAdd;
-    }
-
-    public double substract(double first, double second) {
-        double summeSub = first - second;
-        return summeSub;
-    }
-
-    public double div(double first, double second) {
-        double summeDiv = first / second;
-        return summeDiv;
-    }
-
-    public double multiple(double first, double second) {
-        double summeMul = first * second;
-        return summeMul;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -1,15 +1,35 @@
-package ru.job4j.calculator;
+package ru.job4j.calculate;
 
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class CalculatorTest  {
+/**
+ * Test.
+ *
+ * @author Skakun Maxim (lackdoc@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CalculateTest {
+    /**
+     * Test add.
+     */
     @Test
-    public void whenAddOnePlusOneThenTwo()  {
-        Calculator calc = new Calculator();
-        double result = calc.add(1D, 1D);
-        double expected = 2D;
-        assertThat(result, is(expected));
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(
+                out.toString(),
+                is(
+                        String.format(
+                                "Hello World%s",
+                                System.getProperty("line.separator")
+                        )
+                )
+        );
     }
 }
