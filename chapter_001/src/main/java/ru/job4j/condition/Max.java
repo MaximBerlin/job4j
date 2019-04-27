0ea87ff65733faea6ca8f55e71fd65ebafdad37e
MaximBerlin <lackdoc@yandex.ru>
2019-04-27 03:33:36 +0200

    4.4. Максимум из трех чисел[#135005]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -3,7 +3,6 @@ package ru.job4j.condition;
 public class Max {
     public int max(int left, int right) {
         boolean comparison = left > right;
-        int result = comparison ? left : right;
-        return result;
+        return comparison ? left : right;
     }
 }
