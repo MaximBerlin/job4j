ef11902e5e90f88bb2b5a40c60d5de401649566e
MaximBerlin <lackdoc@yandex.ru>
2019-04-28 18:50:47 +0200

    1.3.1 Maven Plugins[#135040]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -51,10 +51,6 @@ public class Triangle {
      * @return true or false
      */
     private boolean exist(double a, double c, double b) {
-        if (a + b > c && a + c > b && c + b > a) {
-            return true;
-        } else {
-            return false;
-        }
+        return a + b > c && a + c > b && c + b > a;
     }
 }
