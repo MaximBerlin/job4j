d190e486acc4b8af55df0f14b229288e5de4b79d
MaximBerlin <lackdoc@yandex.ru>
2019-05-05 14:49:17 +0200

    6.4. Слово начинается с ...[#135021]

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+/**
+ * Обертка над строкой.
+ */
+public class BubbleSort {
+    boolean result = true;
+
+    public int[] sort(int[] array) {
+        for(int index = 0;index < array.length - 1 ;index++){
+            for(int a = 0;a < array.length - 1;a++){
+                int temp = array[a];
+                if(array[a] > array[a + 1]){
+                    array[a] = array[a + 1];
+                    array[a + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
