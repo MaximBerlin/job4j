d5b7621fe2cc62c1bf1d6d1b686118821ecc9c8c
MaximBerlin <skakun_maxim@gmx.de>
2019-07-19 12:47:41 +0200

    Объединить два массива

--- chapter_001/src/test/java/ru/job4j/array/AddSortedArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/AddSortedArrayTest.java
@@ -10,7 +10,7 @@ public class AddSortedArrayTest {
     public void summArrayLangth (){
         // тест если первый чортированный массив имеет длину 3, от 0 до 2
         // а проторой отсортированный массив имеет длину 4, от 3 до 6
-        // то и длинна их общего массива должна быть 7, от 0 до 6
+        //  то и длинна их общего массива должна быть 7, от 0 до 6
         AddSortedArray addSortedArray = new AddSortedArray();
         int[] inputA = {0,1,2};
         int[] inputB = {3,4,5,6};
