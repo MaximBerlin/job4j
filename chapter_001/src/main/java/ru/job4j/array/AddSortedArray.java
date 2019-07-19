9bba0fcbe0926cda516f854e6ef44b576109db8c
MaximBerlin <skakun_maxim@gmx.de>
2019-07-19 12:44:32 +0200

    Объединить два массива

---
+++ chapter_001/src/main/java/ru/job4j/array/AddSortedArray.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class AddSortedArray {
+
+    public static int[] twoArraysOnOne(int a[], int b[]) {
+        int together[] = new int[a.length + b.length];
+        int indexOne = 0;
+        int indexTwo = 0;
+        int counter = 0;
+
+        while(counter < a.length && counter < b.length) {
+            if (a[indexOne] < b[indexTwo]) {
+                together[counter] = a[indexOne];
+                indexOne++;
+            } else {
+                together[counter] = b[indexTwo];
+                indexTwo++;
+            } counter++;
+        }
+        while(indexTwo < b.length){
+            together[counter] = b[indexTwo];
+            indexTwo++;
+            counter++;
+        }
+
+            return together;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/AddSortedArrayTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class AddSortedArrayTest {
+    @Test
+    public void summArrayLangth (){
+        // тест если первый чортированный массив имеет длину 3, от 0 до 2
+        // а проторой отсортированный массив имеет длину 4, от 3 до 6
+        // то и длинна их общего массива должна быть 7, от 0 до 6
+        AddSortedArray addSortedArray = new AddSortedArray();
+        int[] inputA = {0,1,2};
+        int[] inputB = {3,4,5,6};
+        int[] result = addSortedArray.twoArraysOnOne(inputA,inputB);
+        int[] expect = new int[] {0,1,2,3,4,5,6};
+        assertThat(result, is(expect));
+    }
+}
