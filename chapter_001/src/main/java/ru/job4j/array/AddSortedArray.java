4cae3d6c0f37025fd61aa4c5db9ab7faa0c9d479
MaximBerlin <skakun_maxim@gmx.de>
2019-07-30 23:51:31 +0200

    Объединить два массива

--- chapter_001/src/main/java/ru/job4j/array/AddSortedArray.java
+++ chapter_001/src/main/java/ru/job4j/array/AddSortedArray.java
@@ -4,29 +4,28 @@ public class AddSortedArray {
 
     public static int[] twoArraysOnOne(int[] a, int[] b) {
         int[] together = new int[a.length + b.length];
-        int first = 0;
-        int second = 0;
-        int counter = 0;
-        while (counter < a.length && counter < b.length) {
-            if (a[first] < b[second]) {
-                together[counter] = a[first];
-                first++;
-            } else {
-                together[counter] = b[second];
+         int first = 0;
+         int second = 0;
+        for (int i = 0; i < together.length; i++) {
+            if (first < a.length && second < b.length) {
+                if (a[first] > b[second]) {
+                    together[i] = b[second];
+                    second++;
+                }
+                else {
+                    together[i] = a[first];
+                    first++;
+                }
+            }
+            else if (second < b.length) {
+                together[i] = b[second];
                 second++;
             }
-                counter++;
+            else {
+                together[i] = a[first];
+                first++;
+            }
         }
-        while (second < b.length) {
-            together[counter] = b[second];
-            second++;
-            counter++;
-        }
-        while (first < a.length) {
-            together[counter] = a[first];
-            first++;
-            counter++;
-        }
-            return together;
+        return together;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/AddSortedArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/AddSortedArrayTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 
 public class AddSortedArrayTest {
     @Test
-    public void summArrayLangth() {
+    public void secondArrayLonger() {
         // тест если первый чортированный массив имеет длину 3, от 0 до 2
         // а проторой отсортированный массив имеет длину 4, от 3 до 6
         //  то и длинна их общего массива должна быть 7, от 0 до 6
@@ -19,7 +19,7 @@ public class AddSortedArrayTest {
         assertThat(result, is(expect));
     }
     @Test
-    public void summArraysFerstArrayLonger() {
+    public void firstArrayLonger() {
         AddSortedArray addSortedArray = new AddSortedArray();
         int[] inputA = {3, 4, 5, 6};
         int[] inputB = {0, 1, 2};
@@ -34,5 +34,33 @@ public class AddSortedArrayTest {
         int[] inputB = {2, 4};
         int[] result = addSortedArray.twoArraysOnOne(inputA, inputB);
         int[] expect = new int[] {1, 2, 3, 4};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void firstArrayNull() {
+        AddSortedArray addSortedArray = new AddSortedArray();
+        int[] inputA = {};
+        int[] inputB = {1, 2, 3, 4};
+        int[] result = addSortedArray.twoArraysOnOne(inputA, inputB);
+        int[] expect = new int[] {1, 2, 3, 4};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void secondArrayNull() {
+        AddSortedArray addSortedArray = new AddSortedArray();
+        int[] inputA = {5, 6, 7, 8};
+        int[] inputB = {};
+        int[] result = addSortedArray.twoArraysOnOne(inputA, inputB);
+        int[] expect = new int[] {5, 6, 7, 8};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void bothArraysNull() {
+        AddSortedArray addSortedArray = new AddSortedArray();
+        int[] inputA = {};
+        int[] inputB = {};
+        int[] result = addSortedArray.twoArraysOnOne(inputA, inputB);
+        int[] expect = new int[] {};
+        assertThat(result, is(expect));
     }
 }
